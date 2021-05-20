ed567136ed57d7fed0b9e5342e0afbfe96bf5fe2
Daniel Lopez <daniel.lopezh@student-cs.fr>
2021-05-20 16:19:28 +0200

    added generated news

--- fakenewsdetec/model/tf-idf.py
+++ fakenewsdetec/model/tf-idf.py
@@ -111,7 +111,9 @@ if __name__ == "__main__":
         'train_on': 'text'
     }
 
-    train = pd.read_csv('../../data/train_berkeley_1.csv')
+    generated = table = pd.read_csv('../../data/generated_news.csv')
+    generated['label'] = 1
+    train = pd.concat([pd.read_csv('../../data/train_berkeley_1.csv'), generated])
     test = pd.read_csv('../../data/test_berkeley_1.csv')
     val = pd.read_csv('../../data/eval_berkeley_1.csv')
 
